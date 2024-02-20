24ba325bce94650a27cad90cd848e2798eebee3b
Jimmy <myjimmy@protonmail.com>
2024-02-20 08:45:21 -0800

    JAVASCRIPT: Added the code to retrieve, append, sort, and re-store the data using the local storage.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const newPeriodFormEl = document.getElementsByTagName("form")[0];
 const startDateInputEl = document.getElementById("start-date");
 const endDateInputEl = document.getElementById("end-date");
 
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "period-tracker";
+
 // Listen to form submissions.
 newPeriodFormEl.addEventListener("submit", (event) => {
     // Prevent the form from submitting to the server
@@ -45,4 +48,32 @@ function checkDatesInvalid(startDate, endDate) {
     }
     // else
     return false;
+}
+
+function storeNewPeriod(startDate, endDate) {
+    // Get data from storage.
+    const periods = getAllStoredPeriods();
+
+    // Add the new period objet to the end of the array of period objects.
+    periods.push({ startDate, endDate });
+
+    // Sort the array so that periods are ordered by start date, from newest
+    // to oldest.
+    periods.sort((a, b) => {
+        return new Date(b.startDate) - new Date(a.startDate);
+    });
+
+    // Store the updated array back in the storage.
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+}
+
+function getAllStoredPeriods() {
+    // Get the string of period data from localStorage
+    const data = window.localStorage.getItem(STORAGE_KEY);
+
+    // If no periods were stored, default to an empty array
+    // otherwise, return the stored data as parsed JSON
+    const periods = data ? JSON.parse(data) : [];
+
+    return periods;
 }
