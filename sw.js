14521d7936433c1fb9de1680243dadbe1e88cc0c
Jimmy <myjimmy@protonmail.com>
2024-02-21 00:30:39 -0800

    SERVICE-WORKER: Added the offline resource list.

--- sw.js
+++ sw.js
@@ -1,2 +1,13 @@
 // The version of the cache.
 const VERSION = "v1";
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+    "/",
+    "/index.html",
+    "/app.js",
+    "/cycletrack.json",
+    "/style.css",
+    "/icons/wheel.svg",
+];
+
