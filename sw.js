4354d0ba4f560526e07cc81f242ba5d3616eefb1
Jimmy <myjimmy@protonmail.com>
2024-02-21 00:41:56 -0800

    SERVICE-WORKER: Added the application cache name.

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 // The version of the cache.
 const VERSION = "v1";
 
+// The name of the cache
+const CACHE_NAME = `period-tracker-${VERSION}`;
+
 // The static resources that the app needs to function.
 const APP_STATIC_RESOURCES = [
     "/",
